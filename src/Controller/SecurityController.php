ec6c2cd11f8bccff5ac030e487c6d7f865846552
maikdejong <82510647+maikdejong@users.noreply.github.com>
2024-10-03 15:17:14 +0200

    invalid credentials login error

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -11,8 +11,10 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 use SymfonyCasts\Bundle\VerifyEmail\VerifyEmailHelperInterface;
 
@@ -98,13 +100,19 @@ class SecurityController extends AbstractController
     }
 
     #[Route("/login", name: "app_login")]
-    public function login()
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->getUser()) {
             return $this->redirectToRoute('app_homepage');
         }
 
-        return $this->render('security/login.html.twig');
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
     }
 
     #[Route("/logout", name: "app_logout")]
--- zDocumentatie/ToDo.md
+++ zDocumentatie/ToDo.md
@@ -1,13 +1,21 @@
 # User registration/login
 
+
+## DOING:
+
+- duplicate email: email exists already blabla, FORGOT PASSWORD? -> forgotpassword
+
+
+
 ## TODO:
 
 - Email verification to register, testen met MailHog (functie bestaat al)
 - optional 2fa
-- duplicate email: email exists already blabla, FORGOT PASSWORD? -> forgotpassword
 - Wachtwoord vergeten -> email met nieuw wachtwoord -> verificatie -> nieuwe wachtwoord op de user zetten
 - Flashes in de base.html.twig stylen
 - CustomLoginAuthenticator oid, alleen inloggen wanneer $user->getVerified = true.
+
+
 
 ## DONE:
 
