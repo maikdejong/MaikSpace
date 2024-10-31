1c7089e2249d6005e9338c61e543525d1bcd3c98
Maik de Jong <maik-de-jong@live.nl>
2024-10-31 12:44:10 +0100

    Post wip

---
+++ src/Controller/PostController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Form\PostType;
+use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route('/post')]
+class PostController extends AbstractController
+{
+    #[Route('/', name: 'app_post_index', methods: ['GET'])]
+    public function index(PostRepository $postRepository): Response
+    {
+        return $this->render('post/index.html.twig', [
+            'posts' => $postRepository->findBy([], ['createdAt' => 'DESC'])
+        ]);
+    }
+
+    #[Route('/new', name: 'app_post_new', methods: ['GET', 'POST'])]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        // Check if user is verified
+        if (!$this->getUser()->isVerified()) {
+            $this->addFlash('error', 'Je moet eerst je email verifiëren voordat je posts kunt maken.');
+            return $this->redirectToRoute('app_post_index');
+        }
+
+        $post = new Post();
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post->setUser($this->getUser());
+            $post->setCreatedAt(new \DateTimeImmutable());
+
+            // Handle image upload
+            $imageFile = $form->get('imageFile')->getData();
+            if ($imageFile) {
+                $post->setImage(file_get_contents($imageFile->getPathname()));
+            }
+
+            $entityManager->persist($post);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_post_index');
+        }
+
+        return $this->render('post/new.html.twig', [
+            'post' => $post,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_post_show', methods: ['GET'])]
+    public function show(Post $post): Response
+    {
+        return $this->render('post/show.html.twig', [
+            'post' => $post,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_post_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Post $post, EntityManagerInterface $entityManager): Response
+    {
+        // Check if user is the owner of the post
+        if ($post->getUser() !== $this->getUser()) {
+            throw $this->createAccessDeniedException('Je kunt alleen je eigen posts bewerken.');
+        }
+
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Handle image upload
+            $imageFile = $form->get('imageFile')->getData();
+            if ($imageFile) {
+                $post->setImage(file_get_contents($imageFile->getPathname()));
+            }
+
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_post_show', ['id' => $post->getId()]);
+        }
+
+        return $this->render('post/edit.html.twig', [
+            'post' => $post,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_post_delete', methods: ['POST'])]
+    public function delete(Request $request, Post $post, EntityManagerInterface $entityManager): Response
+    {
+        // Check if user is the owner of the post
+        if ($post->getUser() !== $this->getUser()) {
+            throw $this->createAccessDeniedException('Je kunt alleen je eigen posts verwijderen.');
+        }
+
+        if ($this->isCsrfTokenValid('delete'.$post->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($post);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_post_index');
+    }
+
+    #[Route('/{id}/image', name: 'app_post_image', methods: ['GET'])]
+    public function getImage(Post $post): Response
+    {
+        if (!$post->getImage()) {
+            throw $this->createNotFoundException('Geen afbeelding gevonden.');
+        }
+
+        return new Response(
+            $post->getImage(),
+            Response::HTTP_OK,
+            ['Content-Type' => 'image/jpeg'] // Pas dit aan naar het juiste content type indien nodig
+        );
+    }
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Scheb\TwoFactorBundle\Model\Totp\TotpConfiguration;
 use Scheb\TwoFactorBundle\Model\Totp\TotpConfigurationInterface;
@@ -46,6 +48,17 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface, TwoFact
 
     #[ORM\Column(nullable: true)]
     private ?bool $isTotpEnabled = false;
+
+    /**
+     * @var Collection<int, Post>
+     */
+    #[ORM\OneToMany(targetEntity: Post::class, mappedBy: 'user', orphanRemoval: true)]
+    private Collection $posts;
+
+    public function __construct()
+    {
+        $this->posts = new ArrayCollection();
+    }
 
 
     public function getId(): ?int
@@ -171,6 +184,35 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface, TwoFact
     public function setIsTotpEnabled(?bool $isTotpEnabled): self
     {
         $this->isTotpEnabled = $isTotpEnabled;
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Post>
+     */
+    public function getPosts(): Collection
+    {
+        return $this->posts;
+    }
+
+    public function addPost(Post $post): static
+    {
+        if (!$this->posts->contains($post)) {
+            $this->posts->add($post);
+            $post->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removePost(Post $post): static
+    {
+        if ($this->posts->removeElement($post)) {
+            if ($post->getUser() === $this) {
+                $post->setUser(null);
+            }
+        }
+
         return $this;
     }
 }
