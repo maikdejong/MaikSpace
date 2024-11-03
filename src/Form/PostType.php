16eab0e8efd1c64337ad313647a6014c44e1a9dc
maikdejong <82510647+maikdejong@users.noreply.github.com>
2024-11-03 13:52:01 +0100

    New Post window

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -2,7 +2,31 @@
 
 namespace App\Form;
 
-class PostType
-{
+use App\Entity\Post;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
+class PostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('content', TextareaType::class, [
+                'required' => false,
+            ])
+            ->add('image', FileType::class, [
+                'required' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Post::class,
+        ]);
+    }
 }
