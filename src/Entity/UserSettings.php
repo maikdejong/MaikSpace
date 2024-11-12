c392f0f08fc7f076374d3e9402888c2e18910263
maikdejong <82510647+maikdejong@users.noreply.github.com>
2024-11-12 19:38:56 +0100

    userSetting wip

---
+++ src/Entity/UserSettings.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserSettingsRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserSettingsRepository::class)]
+class UserSettings
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $bio = null;
+
+    #[ORM\Column(type: Types::BLOB, nullable: true)]
+    private $profile_picture;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $username = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBio(): ?string
+    {
+        return $this->bio;
+    }
+
+    public function setBio(?string $bio): static
+    {
+        $this->bio = $bio;
+
+        return $this;
+    }
+
+    public function getProfilePicture()
+    {
+        return $this->profile_picture;
+    }
+
+    public function setProfilePicture($profile_picture): static
+    {
+        $this->profile_picture = $profile_picture;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(?string $username): static
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+}
